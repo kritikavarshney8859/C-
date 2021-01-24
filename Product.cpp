b109ecce9433d59c0482933998039385b9c50f55
kritikavarshney8859 <68337183+kritikavarshney8859@users.noreply.github.com>
2021-01-24 14:17:13 +0530

    Add files via upload

---
+++ Product.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+using namespace::std;
+int main ()
+{
+    float x,y;
+    cout << "Enter your first number\n";
+    cin >> x;
+    cout<< "Enter second number\n";
+    cin>> y;
+    cout<< x*y ;
+    return 0;
+}
---
+++ size.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+     cout<<"Size of char: 1byte\nSize of int: 4bytes\nSize of float: 4bytes\nSize of double: 8 bytes\n";
+    return 0;
+}
